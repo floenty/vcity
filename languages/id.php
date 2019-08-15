8a6176ac95b2d2872612aef6e6f8c3ee97e70769
floenty <54136148+floenty@users.noreply.github.com>
2019-08-15 16:58:55 +0700

    Add files via upload

---
+++ languages/id.php
@@ -0,0 +1,329 @@
+<?php
+//------------------------------------------------------------------------------ 
+// English (EN)
+//------------------------------------------------------------------------------ 
+$arrLang = array();
+
+//Index
+$arrLang['home']             = "Beranda";
+$arrLang['about']            = "Tentang Permainan";
+$arrLang['screenshots']      = "Screenshots";
+$arrLang['screenshots_full'] = "Screenshots layar dari Game";
+$arrLang['signin']           = "Masuk";
+$arrLang['register']         = "Daftar";
+$arrLang['register_full']    = "Daftar sekarang";
+$arrLang['navigation']       = "Navigasi";
+$arrLang['username']         = "Nama pengguna";
+$arrLang['password']         = "Kata sandi";
+$arrLang['email']            = "Alamat email";
+$arrLang['languages']        = "Bahasa";
+$arrLang['themes']           = "Tema";
+$arrLang['default']          = "Default";
+$arrLang['online_players']   = "Pemain Online";
+$arrLang['total_players']    = "Total Pemain";
+$arrLang['best_player']      = "Pemain Terbaik";
+$arrLang['newest_player']    = "Pemain Terbaru";
+$arrLang['admin-panel']      = "Admin Panel";
+
+$arrLang['userpass_incorrect'] = "The entered <strong>Username</strong> or <strong>Password</strong> is incorrect.";
+$arrLang['username_taken']     = "Nama pengguna ini sudah dipakai atau nama pengguna kurang dari 6 karakter";
+$arrLang['email_taken']        = "Alamat E-Mail ini sudah digunakan";
+
+//Core
+$arrLang['vehicles']       = "Kendaraan";
+$arrLang['properties']     = "Properti";
+$arrLang['pets']           = "Peliharaan";
+$arrLang['shop']           = "Toko";
+$arrLang['work']           = "Kerja";
+$arrLang['gym']            = "Gym";
+$arrLang['school']         = "Sekolah";
+$arrLang['bank']           = "Bank";
+$arrLang['hospital']       = "Rumah Sakit";
+$arrLang['street-races']   = "Balapan";
+$arrLang['fights']         = "Pertarungan";
+$arrLang['leaderboard']    = "Pemain Terbaik";
+$arrLang['casino']         = "Casino";
+$arrLang['resources']      = "Isi Ulang";
+$arrLang['money']          = "Uang";
+$arrLang['gold']           = "Emas";
+$arrLang['diamond']        = "Berlian";
+$arrLang['level']          = "Level";
+$arrLang['respect']        = "Respect";
+$arrLang['messages']       = "Pesan";
+$arrLang['my-account']     = "Akun Saya";
+$arrLang['logout']         = "Keluar";
+$arrLang['health']         = "Kesehatan";
+$arrLang['energy']         = "Energi";
+$arrLang['power']          = "Power";
+$arrLang['agility']        = "Kelincahan";
+$arrLang['endurance']      = "Daya tahan";
+$arrLang['intelligence']   = "Kecerdasan";
+$arrLang['useful-tips']    = "Tips Berguna";
+$arrLang['online-players'] = "Pemain Online";
+$arrLang['chat']           = "Chat";
+$arrLang['send']           = "Kirim";
+$arrLang['close']          = "Tutup";
+$arrLang['okay']           = "Oke";
+
+//Home & Player
+$arrLang['your-statistics']     = "Statistik Anda";
+$arrLang['online']              = "Online";
+$arrLang['fight-wins']          = "Fight Wins";
+$arrLang['race-wins']           = "Race Wins";
+$arrLang['income']              = "Pendapatan";
+$arrLang['characters-skills']   = "Keterampilan Karakter";
+$arrLang['your-items']          = "Barang Anda";
+$arrLang['comments']            = "Komentar";
+$arrLang['delete-this-comment'] = "Hapus komentar ini";
+$arrLang['comment']             = "Komentar";
+$arrLang['write-a-comment']     = "Tulis komentar";
+$arrLang['your-home']           = "Rumah Anda";
+$arrLang['home-upgrades']       = "Tingkatkan Rumah";
+$arrLang['your-garage']         = "Garasi Anda";
+$arrLang['garage-upgrades']     = "Peningkatan Garasi";
+$arrLang['your-hangar']         = "Hangar Anda";
+$arrLang['hangar-upgrades']     = "Hangar Upgrades";
+$arrLang['your-quay']           = "Your Quay";
+$arrLang['quay-upgrades']       = "Quay Upgrades";
+$arrLang['sell']                = "Menjual";
+$arrLang['upgrade']             = "Meningkatkan";
+$arrLang['speed']               = "Kecepatan";
+$arrLang['acceleration']        = "Percepatan";
+$arrLang['stability']           = "Stabilitas";
+$arrLang['yno-items']           = "Anda tidak memiliki item dalam inventaris Anda";
+$arrLang['buy-items']           = "Beli Barang";
+$arrLang['selling-vehicle']     = "Jual Kendaraan";
+$arrLang['success-soldv']       = "Anda berhasil menjual kendaraan ini";
+$arrLang['no-comments']         = "Saat ini tidak ada komentar";
+$arrLang['yno-home']            = "Anda tidak memiliki rumah tempat tinggal karakter Anda";
+$arrLang['buy-home']            = "Beli Rumah";
+$arrLang['yno-vehicles']        = "Anda tidak memiliki kendaraan di garasi";
+$arrLang['buy-vehicle']         = "Beli Kendaraan";
+$arrLang['buy-garage']          = "Beli Garasi";
+$arrLang['yno-planes']          = "Anda tidak memiliki pesawat di hangar";
+$arrLang['buy-plane']           = "Beli Pesawat";
+$arrLang['buy-hangar']          = "Beli Hangar";
+$arrLang['yno-boats']           = "Anda tidak punya kapal di dermaga";
+$arrLang['buy-boat']            = "Beli Perahu";
+$arrLang['buy-quay']            = "Beli sebuah dermaga";
+$arrLang['s-statistics']        = "'s Statistics";
+$arrLang['s-items']             = "'s Items";
+$arrLang['s-home']              = "'s Home";
+$arrLang['s-garage']            = "'s Garage";
+$arrLang['s-hangar']            = "'s Hangar";
+$arrLang['s-quay']              = "'s Quay";
+$arrLang['view-all-items']      = "Lihat Semua Item";
+$arrLang['pno-items']           = "Pemain tidak memiliki item dalam inventaris";
+$arrLang['pno-home']            = "Pemain tidak memiliki rumah";
+$arrLang['pno-vehicles']        = "Pemain tidak memiliki kendaraan di garasi";
+$arrLang['pno-planes']          = "Pemain tidak memiliki pesawat di hangar";
+$arrLang['pno-boats']           = "Pemain tidak memiliki kapal di dermaga";
+
+//Vehicles
+$arrLang['required-level']    = "Level yang Diperlukan";
+$arrLang['previous']          = "Sebelumnya";
+$arrLang['next']              = "Berikutnya";
+$arrLang['vno-freeslots']     = "Anda tidak memiliki slot gratis yang cukup untuk membeli kendaraan ini";
+$arrLang['vehicle-showroom']  = "Showroom Kendaraan";
+$arrLang['success-vpurchase'] = "Anda berhasil membeli kendaraan ini";
+$arrLang['owned']             = "Dimiliki";
+$arrLang['buy']               = "Membeli";
+$arrLang['cno-vehicles']      = "Tidak ada kendaraan di kategori ini";
+$arrLang['vip-only']          = "Hanya VIP";
+
+//Properties
+$arrLang['buying-property']   = "Membeli Properti";
+$arrLang['success-ppurchase'] = "Anda berhasil membeli properti ini";
+$arrLang['property-profit']   = "Keuntungan Properti";
+$arrLang['property-details']  = "Detail Properti";
+$arrLang['income-period']     = "Periode Penghasilan";
+$arrLang['take-profit']       = "Mengambil keuntungan";
+$arrLang['every']             = "Setiap";
+
+//Pets
+$arrLang['pets-shop']     = "Toko Hewan";
+$arrLang['bonus']         = "Bonus";
+$arrLang['adopt']         = "Mengambil";
+$arrLang['adoption']      = "Adopsi Peliharaan";
+$arrLang['adopt-nospace'] = "Anda tidak memiliki cukup ruang kosong untuk mengadopsi hewan peliharaan ini";
+$arrLang['adopt-success'] = "Anda berhasil mengadopsi hewan peliharaan ini";
+$arrLang['pet-return']    = "Anda berhasil mengembalikan hewan peliharaan ini";
+$arrLang['pet-details']   = "Detail Hewan Peliharaan";
+$arrLang['return-back']   = "Kembali";
+
+//Shop
+$arrLang['item-spurchase'] = "Anda berhasil membeli item ini";
+$arrLang['item-ssold']     = "Anda berhasil menjual item ini";
+$arrLang['buy-use']        = "Beli & Gunakan";
+$arrLang['item-details']   = "Rincian Barang";
+$arrLang['single-use']     = "Single Use";
+
+//Work
+$arrLang['jobs']        = "Pekerjaan";
+$arrLang['wday-end']    = "Akhir Hari Kerja";
+$arrLang['work-finish'] = "Anda selesai bekerja dan menerima gaji";
+$arrLang['claim']       = "Klaim";
+$arrLang['energy-cost'] = "Biaya Energi";
+$arrLang['health-loss'] = "Biaya Kesehatan";
+$arrLang['work-time']   = "Waktu kerja";
+$arrLang['salary']      = "Gaji";
+$arrLang['reward']      = "Penghargaan";
+$arrLang['get-salary']  = "Dapatkan Gaji";
+$arrLang['start-work']  = "Mulai bekerja";
+$arrLang['leave-work']  = "Meninggalkan pekerjaan";
+
+//Gym
+$arrLang['end-workout']        = "Akhir dari Latihan";
+$arrLang['finish-workout']     = "Anda telah berhasil menyelesaikan latihan";
+$arrLang['training-duration']  = "Durasi Pelatihan";
+$arrLang['fitness-fee']        = "Kartu Kebugaran - Biaya";
+$arrLang['health-restore']     = "Pemulihan Kesehatan";
+$arrLang['finish-workoutn']    = "Finish Workout";
+$arrLang['start-workout']      = "Mulai Berolahraga";
+$arrLang['interrupt-training'] = "Pelatihan Interrupt";
+
+//School
+$arrLang['end-education']       = "Akhir dari Kursus Pendidikan";
+$arrLang['finish-edu']          = "Anda telah berhasil menyelesaikan Kursus Pendidikan";
+$arrLang['lesson-duration']     = "Durasi Pelajaran";
+$arrLang['education-fee']       = "Biaya Pendidikan";
+$arrLang['finish-education']    = "Selesaikan Pendidikan";
+$arrLang['start-education']     = "Mulai Pendidikan";
+$arrLang['interrupt-education'] = "Batalkan Pendidikan";
+
+//Bank
+$arrLang['deposited-money']   = "Setor Uang";
+$arrLang['deposit-money']     = "Uang deposito";
+$arrLang['withdraw-money']    = "Tarik Uang";
+$arrLang['deposit']           = "Deposito";
+$arrLang['withdraw']          = "Menarik";
+$arrLang['depositing-money']  = "Menyetor Uang";
+$arrLang['withdrawing-money'] = "Menarik Uang";
+$arrLang['invalid-amount']    = "Jumlah uang tidak valid";
+$arrLang['noenough-money']    = "Anda tidak memiliki jumlah uang sebanyak itu";
+$arrLang['minimum-amount']    = "Jumlah minimum yang diizinkan adalah";
+$arrLang['bank-full']         = "Kotak deposit sudah penuh";
+$arrLang['success-deposit']   = "Berhasil disimpan";
+$arrLang['insuff-av']         = "Ketersediaan Tidak mencukupi";
+$arrLang['success-withdrawn'] = "Berhasil ditarik";
+
+//Hospital
+$arrLang['end-treatment']       = "Akhir dari Perawatan";
+$arrLang['finished-treatment']  = "Anda telah berhasil menyelesaikan Perawatan";
+$arrLang['treatment-duration']  = "Durasi pengobatan";
+$arrLang['treatment-cost']      = "Biaya Perawatan";
+$arrLang['finish-treatment']    = "Selesaikan Pengobatan";
+$arrLang['interrupt-treatment'] = "Perawatan Interupsi";
+$arrLang['start-treatment']     = "Mulai Perawatan";
+
+//Races
+$arrLang['you-win']         = "Anda memenangkan perlombaan.";
+$arrLang['and']             = "dan";
+$arrLang['you-lose']        = "Anda kalah dalam balapan";
+$arrLang['no-energy']       = "Anda tidak memiliki energi yang cukup untuk berlomba. Anda harus menunggu energi Anda dipulihkan";
+$arrLang['no-health1']      = "Anda tidak cukup sehat untuk berlomba. Anda harus pergi ke";
+$arrLang['no-health2']      = "untuk memulihkan kesehatan Anda";
+$arrLang['no-racers']       = "Tidak ada pemain lain di Zona Balap saat ini.";
+$arrLang['select-vehicle']  = "Pilih Kendaraan";
+$arrLang['select-vehicle2'] = "Pilih kendaraan";
+$arrLang['chance-win']      = "Kesempatan untuk menang";
+$arrLang['little']          = "Sedikit";
+$arrLang['average']         = "Rata-rata";
+$arrLang['big']             = "Besar";
+$arrLang['find-another']    = "Cari yang lain";
+$arrLang['race']            = "Race";
+$arrLang['cant-racemore']   = "Anda tidak dapat berlomba melawan pemain ini lebih dari tiga kali untuk hari ini.";
+
+//Fights
+$arrLang['fight-arena']    = "Arena Pertarungan";
+$arrLang['cant-fightmore'] = "Anda tidak dapat bertarung melawan pemain ini lebih dari tiga kali untuk hari ini.";
+$arrLang['you-winf']       = "Anda memenangkan pertarungan melawan pemain ini.";
+$arrLang['you-losef']      = "Anda kalah dalam pertarungan melawan pemain ini";
+$arrLang['no-energytf']    = "Anda tidak memiliki energi yang cukup untuk bertarung. Anda harus menunggu energi Anda dipulihkan";
+$arrLang['no-healthtf1']   = "Anda tidak cukup sehat untuk bertarung. Anda harus pergi ke";
+$arrLang['no-fighters']    = "Tidak ada pemain lain di Arena Pertempuran saat ini.";
+$arrLang['fight']          = "Pertarungan";
+
+//Leaderboard
+$arrLang['all-players']  = "Semua Pemain";
+$arrLang['player']       = "Pemain";
+$arrLang['actions']      = "Tindakan";
+$arrLang['visit-player'] = "Kunjungi Player";
+
+//Casino
+$arrLang['no-moneyts']     = "Anda tidak punya cukup uang untuk membeli";
+$arrLang['spins']          = "spins";
+$arrLang['wheel-fortune']  = "Roda keberuntungan";
+$arrLang['buy-spins']      = "Beli Spins";
+$arrLang['spin-prize1']    = "Biaya";
+$arrLang['quantity']       = "Jumlah";
+$arrLang['high']           = "Cepat";
+$arrLang['medium']         = "Sedang";
+$arrLang['low']            = "Pelan";
+$arrLang['spin']           = "Putar";
+$arrLang['spin-prize']     = "Hadiah Anda adalah";
+$arrLang['refresh-tospin'] = "Segarkan halaman jika Anda ingin memutar roda lagi.";
+
+//Isi Ulang
+$arrLang['resource-details'] = "Detail Isi Ulang";
+$arrLang['type']             = "Type";
+$arrLang['amount']           = "Jumlah";
+$arrLang['price']            = "Harga";
+$arrLang['purchase']         = "Membeli";
+$arrLang['energy-refill']    = "Isi Ulang Energi";
+$arrLang['vip-status']       = "Status VIP";
+
+//Settings
+$arrLang['account-settings'] = "Pengaturan akun";
+$arrLang['email-address']    = "Alamat email";
+$arrLang['avatar']           = "Avatar";
+$arrLang['new-password']     = "Kata Sandi Baru";
+$arrLang['save']             = "Menyimpan";
+$arrLang['password-message'] = "Isi bidang ini hanya jika Anda ingin mengubah kata sandi Anda.";
+
+//Messages
+$arrLang['message']         = "Pesan";
+$arrLang['sender']          = "Pengirim";
+$arrLang['date']            = "Tanggal";
+$arrLang['at']              = "at";
+$arrLang['sending-message'] = "Mengirim Pesan";
+$arrLang['success-sent']    = "Pesan Anda berhasil dikirim";
+$arrLang['inbox']           = "Kotak masuk";
+$arrLang['send-message']    = "Mengirim pesan";
+$arrLang['view']            = "Melihat";
+$arrLang['delete']          = "Menghapus";
+$arrLang['recipient']       = "Penerima";
+
+//Home Upgrades
+$arrLang['home-up']        = "Upgrade Rumah";
+$arrLang['success-uphome'] = "Anda berhasil meningkatkan rumah Anda";
+$arrLang['home1']          = "Rumah";
+$arrLang['min-level']      = "Level Minimum";
+$arrLang['max-pets']       = "Hewan peliharaan maksimum";
+$arrLang['preview']        = "Pratinjau";
+
+//Garage Upgrades
+$arrLang['garage-up']        = "Pembaruan Garasi";
+$arrLang['success-upgarage'] = "Anda berhasil meningkatkan garasi Anda";
+$arrLang['garage']           = "Garasi";
+$arrLang['max-vehicles']     = "Kendaraan Maksimal";
+
+//Hangar Upgrades
+$arrLang['hangar-up']        = "Peningkatan Hangar";
+$arrLang['success-uphangar'] = "Anda berhasil meningkatkan hangar Anda";
+$arrLang['hangar']           = "Hangar";
+
+//Quay Upgrades
+$arrLang['quay-up']        = "Tingkatkan Dermaga";
+$arrLang['success-upquay'] = "Anda berhasil meningkatkan Dermaga Anda";
+$arrLang['quay']           = "Dermaga";
+
+//Vehicle Upgrades
+$arrLang['vehicle-upgrades']  = "Peningkatan Kendaraan";
+$arrLang['success-upvehicle'] = "Kendaraan saya berhasil ditingkatkan";
+$arrLang['maxed']             = "Maxed";
+
+//Choose Character
+$arrLang['choose-your-character'] = "Pilih Karakter Anda";
+$arrLang['start-the-game']        = "Memulai permainan";
+?>
